0ce9c79ba9335537ff9cad273cd7395e4b3411c5
sorrentmutie <salvatore.sorrentino@live>
2018-09-26 17:07:13 +0200

    Esercitazione ReadLine

---
+++ week1/day3/EsercitazionePomeriggio/EsercitazionePomeriggio/Autenticazione.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercitazionePomeriggio
+{
+    public class Autenticazione
+    {
+        
+        private int UsernameCorretto = 1234;
+        private string PasswordCorretta = "Ciao";
+        public bool Login(int Username, string Password)
+        {
+            if (UsernameCorretto == Username && PasswordCorretta == Password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
--- week1/day3/EsercitazionePomeriggio/EsercitazionePomeriggio/Program.cs
+++ week1/day3/EsercitazionePomeriggio/EsercitazionePomeriggio/Program.cs
@@ -19,18 +19,58 @@ namespace EsercitazionePomeriggio
             //var Classe2 = Classe.Clona();
             //Metodo2(Classe);
             //Console.WriteLine(Classe.Numero1);
-            var A = "42,5";
-            double B; 
-            bool C;
-            //var C = false;
-            C = double.TryParse(A, out B);
-            Console.WriteLine(B);
+            //var A = "42,5";
+            //double B; 
+            //bool C;
+            ////var C = false;
+            //C = double.TryParse(A, out B);
+            //Console.WriteLine(B);
 
 
-
+            // var NumeroMatricola = Console.ReadLine();
+            // Console.WriteLine(NumeroMatricola);
+            Console.WriteLine("Inserire numero matricola ");
+            int numero=0;
+            while (numero==0)
+            {
+                numero = AnalizzaNumeroMatricola(Console.ReadLine());
+            }
+            Console.WriteLine("Inserire password ");
+            string password = Console.ReadLine();
+            // numero = AnalizzaNumeroMatricola(NumeroMatricola);
+            var Autenticazione = new Autenticazione();
+            bool Autenticato = Autenticazione.Login(numero, password);
+            if(Autenticato == false)
+            {
+                Console.WriteLine("Credenziali errate. Il programma interrompe la sua esecuzione");
+            }
+            else
+            {
+                Console.WriteLine("Ok, credenziali giuste");
+            }
 
             Console.ReadLine();
         }
+
+        
+
+        private static int AnalizzaNumeroMatricola(string NumeroMatricola)
+        {
+            int numero;
+            bool numero3;
+            numero3 = int.TryParse(NumeroMatricola, out numero);
+            if (numero3 == false)
+            {
+                Console.WriteLine("Scrivi un numero corretto!!");
+            }
+            else
+            {
+                Console.WriteLine("Vai avanti!");
+            }
+
+            return numero;
+        }
+
         static void Metodo1(string A)
         {
             A = A + "2";
