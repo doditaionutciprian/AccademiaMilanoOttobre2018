7c122013a12ccf5b1b53829e395624b954886574
sorrentmutie <salvatore.sorrentino@live>
2018-09-26 16:01:10 +0200

    reference e value

---
+++ week1/day3/EsercitazionePomeriggio/EsercitazionePomeriggio/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercitazionePomeriggio
+{
+    public class Class1
+    {
+        public int Numero1 { get; set; }
+        public string Nome1 { get; set; }
+        public Class1 Clona()
+        {
+            var Clone = new Class1();
+            Clone.Numero1 = Numero1;
+            Clone.Nome1 = Nome1;
+            return Clone;
+        }
+    }
+}
---
+++ week1/day3/EsercitazionePomeriggio/EsercitazionePomeriggio/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercitazionePomeriggio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string Numero = "Ciao";
+            //Metodo1(Numero);
+            //Console.WriteLine(Numero);
+            //var Classe = new Class1();
+            //Classe.Numero1 = 42;
+            //Classe.Nome1 = "Simone";
+            //var Classe2 = Classe.Clona();
+            //Metodo2(Classe);
+            //Console.WriteLine(Classe.Numero1);
+            var A = "42,5";
+            double B; 
+            bool C;
+            //var C = false;
+            C = double.TryParse(A, out B);
+            Console.WriteLine(B);
+
+
+
+
+            Console.ReadLine();
+        }
+        static void Metodo1(string A)
+        {
+            A = A + "2";
+            Console.WriteLine("il valore di A è "+A);
+        }
+        static void Metodo2(Class1 B)
+        {
+            B.Numero1 = B.Numero1 * 2;
+            B.Nome1 = "Alessio";
+            Console.WriteLine(B.Numero1);
+        }
+    }
+}
