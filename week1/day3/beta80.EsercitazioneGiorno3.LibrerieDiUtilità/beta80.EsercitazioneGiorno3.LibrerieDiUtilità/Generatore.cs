7bc98758fdc789ddde876a05d6c6aaced547dafe
sorrentmutie <salvatore.sorrentino@live>
2018-09-26 11:02:46 +0200

    Generatore Random

---
+++ week1/day3/beta80.EsercitazioneGiorno3.LibrerieDiUtilità/beta80.EsercitazioneGiorno3.LibrerieDiUtilità/Generatore.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beta80.EsercitazioneGiorno3.LibrerieDiUtilità
+{
+    public class Generatore
+    {
+        private Random random = new Random();
+        public int GeneraNumeroIntero(int Minimo, int Massimo)
+        {
+            return random.Next(Minimo, Massimo+1);
+        }
+
+        public decimal GeneraNumeroDecimale(decimal Minimo, decimal Massimo)
+        {
+            var numero = random.NextDouble();
+            return (Minimo + (Massimo - Minimo) * (decimal)numero);
+        }
+
+        public string GeneraLetteraDellAlfabeto()
+        {
+            string Lettere = "abcdefghijklmnopqrstuvwxyz";
+            Lettere = Lettere + Lettere.ToUpper();
+            var Numero = GeneraNumeroIntero(1, Lettere.Length);
+            return Lettere.ElementAt(Numero - 1).ToString();
+        }
+
+        public string GeneraParola(int Lunghezza)
+        {
+            string Parola = "";
+            for (int i = 0; i < Lunghezza; i++)
+            {
+                Parola += GeneraLetteraDellAlfabeto();
+            }
+            return Parola;
+        }
+
+        public bool GeneraBooleano()
+        {
+            return GeneraNumeroIntero(0, 1) == 0;
+        }
+
+        public DateTime GeneraDataCasuale(int AnnoMinimo, int AnnoMassimo)
+        {
+            int Anno = GeneraNumeroIntero(AnnoMinimo, AnnoMassimo);
+            int Mese = GeneraNumeroIntero(1, 12);
+            int NumeroGiorniNelMese = DateTime.DaysInMonth(Anno, Mese);
+            int Giorno = GeneraNumeroIntero(1, NumeroGiorniNelMese);
+            return new DateTime(Anno, Mese, Giorno);
+        }
+    }
+
+
+}
---
+++ week1/day3/beta80.EsercitazioneGiorno3.LibrerieDiUtilità/beta80.EsercitazioneGiorno3.TestLibreria/Program.cs
@@ -0,0 +1,63 @@
+﻿using beta80.EsercitazioneGiorno3.LibrerieDiUtilità;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beta80.EsercitazioneGiorno3.TestLibreria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generatore = new Generatore();
+            Console.WriteLine(generatore.GeneraNumeroIntero(1, 10));
+            var Test = generatore.GeneraNumeroIntero(0, 3);
+            Console.WriteLine(Test);
+            if (Test == 0 || Test == 1)
+            {
+                Console.WriteLine("Test1 ok");
+            }
+
+            else
+            {
+                Console.WriteLine("Test1 fallito ");
+            }
+
+            var Test2 = generatore.GeneraNumeroIntero(1, 100);
+            if(Test2<=100)
+            {
+                Console.WriteLine("Test2 ok");  
+            }
+            else
+            {
+                Console.WriteLine("Test2 fallito");
+            }
+
+            Console.WriteLine(generatore.GeneraBooleano());
+            Console.WriteLine(generatore.GeneraParola(10));
+            Console.WriteLine(generatore.GeneraNumeroDecimale(1.2M,2.3M));
+            Console.WriteLine(generatore.GeneraDataCasuale(2002,2015));
+            int ContatoreTrue = 0;
+            int ContatoreFalse = 0;
+            for(int i=0;i<100;i++)
+            {
+
+                var Generato=generatore.GeneraBooleano();
+                Console.WriteLine(Generato);
+                if (Generato == true)
+                { ContatoreTrue++; }
+                else
+                {
+                    ContatoreFalse++;
+                }              
+            }
+            Console.WriteLine(ContatoreTrue);
+            Console.WriteLine(ContatoreFalse);
+            Console.ReadLine();
+        }
+
+        
+    }
+}
