78da40bf695b65b7d304a1346c5e5126c04e1ffd
sorrentmutie <salvatore.sorrentino@live>
2018-09-27 12:20:35 +0200

    esercizi giorno 4: collezioni

---
+++ week1/day4/EsercitazioneCollezioni/EsercitazioneCollezioni/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+
+namespace EsercitazioneCollezioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Array = new ArrayList();
+
+
+            for (int i = 0; i < 3000; i++)
+            {
+                Array.Add(new Studente { Id = i, Nome = i.ToString() });
+            }
+            Array.Add("Stringa");
+
+            foreach (var item in Array)
+            {
+                if (item.GetType().ToString() == "EsercitazioneCollezioni.Studente")
+                {
+                    var p1 = item as Studente;
+                    p1.FaiQualcosa();
+                }
+            }
+
+            Console.WriteLine(Array[0]);
+
+            //---------------------------------------------------------------------
+            //var Hash = new Hashtable();
+            //for (int i = 0; i < 3000; i++)
+            //{
+            //    //Hash.Add(i, i*5);
+            //    Hash.Add(i, new Studente { Id = i, Nome = i.ToString() });
+            //}
+
+            //Hash.Add("pippo", "topolino");
+            ////Hash.Add("pippo","topolina");
+
+            //var p = Hash[1000];
+            //var c = Hash["pippo"];
+
+            //if (p.GetType().ToString()=="EsercitazioneCollezioni.Studente")
+            //{
+            //    Console.WriteLine("Tipo giusto");
+            //    var p1 = (Studente)p;
+            //    p1.FaiQualcosa();
+            //}
+
+            //Console.WriteLine(p.GetType());
+            //Console.WriteLine(Hash["pippo"]);
+
+            //---------------------------------------------------------------------
+
+            //var Coda = new Queue();
+            //var Stack = new Stack();
+
+            //for (int i = 0; i < 3000; i++)
+            //{
+            //    Coda.Enqueue(new Studente { Id = i, Nome = i.ToString() });
+            //    Stack.Push(new Studente { Id = i, Nome = i.ToString() });
+            //}
+
+            //Coda.Dequeue();
+            //Stack.Pop();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week1/day4/EsercitazioneCollezioni/EsercitazioneCollezioni/Studente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercitazioneCollezioni
+{
+    public class Studente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public override string ToString()
+        {
+            return "Matricola: " + Id + " Nome: " + Nome;
+        }
+
+        public void FaiQualcosa() {
+            Console.WriteLine("qualcosa");
+        }
+    }
+}
